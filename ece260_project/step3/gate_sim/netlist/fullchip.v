8f4aa8e992f46b081ef3f215bf718006824b88dc
Allen Keng <allenkeng8@gmail.com>
2025-03-21 17:40:02 -0700

    feat: xCelium opens to view waveform, core doesn't seem to have connected nets

--- ece260_project/step3/gate_sim/netlist/fullchip.v
+++ ece260_project/step3/gate_sim/netlist/fullchip.v
@@ -12,26 +12,26 @@ input  [pr*bw-1:0] mem_in;
 input  [16:0] inst; 
 input  reset;
 
-output [bw_psum*col-1:0] core_out; // TAHSEEN EDIT 
+//output [bw_psum*col-1:0] core_out; // TAHSEEN EDIT 
 
-core #(.bw(bw), .bw_psum(bw_psum), .col(col), .pr(pr)) core_instance (
+core /*#(.bw(bw), .bw_psum(bw_psum), .col(col), .pr(pr))*/ core_instance (
       .reset(reset), 
       .clk(clk), 
       .mem_in(mem_in), 
-      .inst(inst),
-      .out(core_out)
+      .inst(inst)/*,
+      .out(core_out)*/
 );
 integer q; // Declare loop variable properly
 
 always @(posedge clk) begin
-    if (inst[1]) begin
+    /*if (inst[1]) begin
         $write("Core Output |");
         for (q = col-1; q >= 0; q = q - 1) begin  // Reverse the order to match expected output
             $write(" %6d |", $signed(core_out[q*bw_psum +: bw_psum])); // Convert HEX to signed DECIMAL
         end
         $display(""); // Move to new line
         $display("---------------------------------------------------");
-    end
+    end*/
 end
 
 
---
+++ ece260_project/step3/gate_sim/xcelium.d/run.lnx8664.20.09.d/.timestamp.ts
@@ -0,0 +1,7 @@
+1737007846 /home/linux/ieng6/ee260bwi25/public/PDKdata/verilog/tcbn65gplus.v
+1742602705 /home/linux/ieng6/ee260bwi25/aekeng/ECE260B_FP/ece260_project/step3/gate_sim/netlist/sram_w16_160.pnr.v
+1742603002 /home/linux/ieng6/ee260bwi25/aekeng/ECE260B_FP/ece260_project/step3/gate_sim/netlist/fullchip.v
+1742600455 /home/linux/ieng6/ee260bwi25/aekeng/ECE260B_FP/ece260_project/step3/gate_sim/core.pnr.v
+1742600455 /home/linux/ieng6/ee260bwi25/aekeng/ECE260B_FP/ece260_project/step3/gate_sim/netlist/core.pnr.v
+1742587805 /home/linux/ieng6/ee260bwi25/aekeng/ECE260B_FP/ece260_project/step3/gate_sim/netlist/fullchip_tb.v
+1742602742 /home/linux/ieng6/ee260bwi25/aekeng/ECE260B_FP/ece260_project/step3/gate_sim/netlist/sram_w16.pnr.v
