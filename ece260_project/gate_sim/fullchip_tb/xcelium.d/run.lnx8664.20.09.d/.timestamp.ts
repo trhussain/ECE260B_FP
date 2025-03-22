4e4f5c37a0c321e658ffdb89dea2c5c7bea966eb
idegrood <idegrood@ucsd.edu>
2025-03-22 16:30:48 -0700

    removed xcelium bloat

--- ece260_project/gate_sim/fullchip_tb/xcelium.d/run.lnx8664.20.09.d/.timestamp.ts
+++
@@ -1,7 +0,0 @@
-1737007846 /home/linux/ieng6/ee260bwi25/public/PDKdata/verilog/tcbn65gplus.v
-1742680130 /home/linux/ieng6/ee260bwi25/idegrood/ECE260B_FP/ece260_project/gate_sim/netlist/core.out.v
-1742679982 /home/linux/ieng6/ee260bwi25/idegrood/ECE260B_FP/ece260_project/gate_sim/netlist/fullchip_tb.v
-1742679982 /home/linux/ieng6/ee260bwi25/idegrood/ECE260B_FP/ece260_project/gate_sim/netlist/fifo_depth8.v
-1742679982 /home/linux/ieng6/ee260bwi25/idegrood/ECE260B_FP/ece260_project/gate_sim/netlist/fifo_mux_8_1.v
-1742679982 /home/linux/ieng6/ee260bwi25/idegrood/ECE260B_FP/ece260_project/gate_sim/netlist/fifo_mux_2_1.v
-1742679982 /home/linux/ieng6/ee260bwi25/idegrood/ECE260B_FP/ece260_project/gate_sim/netlist/fullchip.v
--- ece260_project/gate_sim/fullchip_tb_norm_v/xcelium.d/run.lnx8664.20.09.d/files.ts
+++
@@ -1,7 +0,0 @@
-1737007846 /home/linux/ieng6/ee260bwi25/public/PDKdata/verilog/tcbn65gplus.v
-1742681460 /home/linux/ieng6/ee260bwi25/idegrood/ECE260B_FP/ece260_project/gate_sim/fullchip_tb_norm_v/netlist/core.out.v
-1742681460 /home/linux/ieng6/ee260bwi25/idegrood/ECE260B_FP/ece260_project/gate_sim/fullchip_tb_norm_v/netlist/fullchip.v
-1742681460 /home/linux/ieng6/ee260bwi25/idegrood/ECE260B_FP/ece260_project/gate_sim/fullchip_tb_norm_v/netlist/fifo_depth8.v
-1742681460 /home/linux/ieng6/ee260bwi25/idegrood/ECE260B_FP/ece260_project/gate_sim/fullchip_tb_norm_v/netlist/fifo_mux_8_1.v
-1742681460 /home/linux/ieng6/ee260bwi25/idegrood/ECE260B_FP/ece260_project/gate_sim/fullchip_tb_norm_v/netlist/fifo_mux_2_1.v
-1742681460 /home/linux/ieng6/ee260bwi25/idegrood/ECE260B_FP/ece260_project/gate_sim/fullchip_tb_norm_v/netlist/fullchip_tb_norm_v.v
