2b8b5eed0beced8fd0676dc4acdf0f2240d0d40a
trhussain <trhussain@ieng6-ece-16.ucsd.edu>
2025-03-22 19:41:09 -0700

    *.enc mmc files added

--- Step_3/gate_sim/xcelium.d/run.lnx8664.20.09.d/.timestamp.ts
+++ Step_3/gate_sim/xcelium.d/run.lnx8664.20.09.d/.timestamp.ts
@@ -1,6 +1,11 @@
 1737007846 /home/linux/ieng6/ee260bwi25/public/PDKdata/verilog/tcbn65gplus.v
+1742696827 /home/linux/ieng6/ee260bwi25/trhussain/allenTest/ECE260B_FP/Step_3/gate_sim/netlist/sram_w16_160.pnr.v
+1742696827 /home/linux/ieng6/ee260bwi25/trhussain/allenTest/ECE260B_FP/Step_3/gate_sim/netlist/fullchip.v
+1742696827 /home/linux/ieng6/ee260bwi25/trhussain/allenTest/ECE260B_FP/Step_3/gate_sim/netlist/core.pnr.v
+1742696827 /home/linux/ieng6/ee260bwi25/trhussain/allenTest/ECE260B_FP/Step_3/gate_sim/netlist/fullchip_tb.v
 1742692716 /home/linux/ieng6/ee260bwi25/aekeng/ECE260B_FP/ece260_project/step3/gate_sim/netlist/sram_w16_160.pnr.v
 1742692716 /home/linux/ieng6/ee260bwi25/aekeng/ECE260B_FP/ece260_project/step3/gate_sim/netlist/sram_w16_64.pnr.v
 1742692715 /home/linux/ieng6/ee260bwi25/aekeng/ECE260B_FP/ece260_project/step3/gate_sim/netlist/fullchip.v
 1742695512 /home/linux/ieng6/ee260bwi25/aekeng/ECE260B_FP/ece260_project/step3/gate_sim/netlist/core.pnr.v
 1742692715 /home/linux/ieng6/ee260bwi25/aekeng/ECE260B_FP/ece260_project/step3/gate_sim/netlist/fullchip_tb.v
+1742696827 /home/linux/ieng6/ee260bwi25/trhussain/allenTest/ECE260B_FP/Step_3/gate_sim/netlist/sram_w16_64.pnr.v
